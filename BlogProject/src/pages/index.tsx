a3829555b8e08714acd539ff66e97b31c162ecdf
LEE <dlwodyd4529@gmail.com>
2023-03-03 17:43:09 +0900

    Blog Project Start

---
+++ BlogProject/src/pages/index.tsx
@@ -0,0 +1,167 @@
+import React, { FunctionComponent, useMemo } from 'react'
+import styled from '@emotion/styled'
+import GlobalStyle from 'components/Common/GlobalStyle'
+import Footer from 'components/Common/Footer'
+import CategoryList from 'components/Main/CategoryList'
+import Introduction from 'components/Main/Introduction'
+import PostList from 'components/Main/PostList'
+import { graphql } from 'gatsby'
+import { PostListItemType } from 'types/PostItem.types'
+import { IGatsbyImageData } from 'gatsby-plugin-image'
+import queryString, { ParsedQuery } from 'query-string'
+import Template from 'components/Common/Template'
+
+// DO NOT change version of fallowing plugins.
+// "gatsby-transformer-remark": "5.21.0",
+// "gatsby-transformer-sharp": "^4.20.0",
+
+// type IndexPageProps = {
+//   data: {
+//     allMarkdownRemark: {
+//       edges: PostListItemType[]
+//     }
+//     file: {
+//       childImageSharp: {
+//         gatsbyImageData: IGatsbyImageData
+//       }
+//     }
+//   }
+// }
+
+type IndexPageProps = {
+  location: {
+    search: string
+  }
+  data: {
+    allMarkdownRemark: {
+      edges: PostListItemType[]
+    }
+    file: {
+      childImageSharp: {
+        gatsbyImageData: IGatsbyImageData
+      }
+    }
+  }
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`
+
+// const IndexPage: FunctionComponent<IndexPageProps> = function ({
+//   location: { search },
+//   data: {
+//     allMarkdownRemark: { edges },
+//     file: {
+//       childImageSharp: { gatsbyImageData },
+//     },
+//   },
+// }) {
+//   const parsed: ParsedQuery<string> = queryString.parse(search)
+//   const selectedCategory: string =
+//     typeof parsed.category !== 'string' || !parsed.category
+//       ? 'All'
+//       : parsed.category
+
+//   return (
+//     <Container>
+//       <GlobalStyle />
+//       <Introduction profileImage={gatsbyImageData} />
+//       <CategoryList
+//         selectedCategory={selectedCategory}
+//         categoryList={CATEGORY_LIST}
+//       />
+//       <PostList posts={edges} />
+//       <Footer />
+//     </Container>
+//   )
+// }
+
+const IndexPage: FunctionComponent<IndexPageProps> = function ({
+  location: { search },
+  data: {
+    allMarkdownRemark: { edges },
+    file: {
+      childImageSharp: { gatsbyImageData },
+    },
+  },
+}) {
+  const parsed: ParsedQuery<string> = queryString.parse(search)
+  const selectedCategory: string =
+    typeof parsed.category !== 'string' || !parsed.category
+      ? 'All'
+      : parsed.category
+
+  const categoryList = useMemo(
+    () =>
+      edges.reduce(
+        (
+          list: CategoryListProps['categoryList'],
+          {
+            node: {
+              frontmatter: { categories },
+            },
+          }: PostType,
+        ) => {
+          categories.forEach(category => {
+            if (list[category] === undefined) list[category] = 1
+            else list[category]++
+          })
+
+          list['All']++
+
+          return list
+        },
+        { All: 0 },
+      ),
+    [],
+  )
+
+  return (
+    <Template>
+      <Introduction profileImage={gatsbyImageData} />
+      <CategoryList
+        selectedCategory={selectedCategory}
+        categoryList={categoryList}
+      />
+      <PostList selectedCategory={selectedCategory} posts={edges} />
+    </Template>
+  )
+}
+
+export default IndexPage
+
+export const getPostList = graphql`
+  query getPostList {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date, frontmatter___title] }
+    ) {
+      edges {
+        node {
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            summary
+            date(formatString: "YYYY.MM.DD.")
+            categories
+            thumbnail {
+              childImageSharp {
+                gatsbyImageData(width: 768, height: 400)
+              }
+            }
+          }
+        }
+      }
+    }
+    file(name: { eq: "profile-image" }) {
+      childImageSharp {
+        gatsbyImageData(width: 120, height: 120)
+      }
+    }
+  }
+`
